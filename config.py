649093a6dac55375955b0e40b235324e8bdc8ac0
ceggyweggy <75233208+ceggyweggy@users.noreply.github.com>
2020-12-14 21:15:40 +0800

    Removed tokens and links

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
-TOKEN = '1373278931:AAHDy4LtDnLgJ0k99hlAntiUJAVbWj8Epfk'
-NGROK_URL = 'https://0fa9a03bce45.ngrok.io'
+TOKEN = ''
+NGROK_URL = ''
 BASE_TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
 LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(NGROK_URL)
 TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
